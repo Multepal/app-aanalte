962e9ed77cc9f50c1b724b29b3b665eb3c82563b
Rafael Alvarado <ontoligent@gmail.com>
2022-07-19 12:33:18 -0400

    Upated Drupal content and annotation mapper

--- collections/topics/get-content.py
+++ collections/topics/get-content.py
@@ -3,113 +3,112 @@
 import configparser
 import requests
 import os
-
-
-# Hanlde Config 
-
-config = configparser.ConfigParser()
-config.read('./config.ini')
-
-base_path = config['DEFAULT']['base_path']
-multepal = config['multepal']['multepal_url']
-exports = {'topics':{}, 'annotations':{}, 'snippets':{}}
-
-for export in exports.keys():
-    exports[export] = {
-        'url': multepal + config['multepal']['{}_url'.format(export)],
-        'file': base_path + config['multepal']['{}_src'.format(export)]
-    }
-
-for export in exports.keys():
-    print("Getting export from Multepal site")
-    url = exports[export]['url']
-    print(url)
-    r = requests.get(url)
-    out_file = exports[export]['file']
-    print('out_file', out_file)
-    if os.path.exists(out_file):
-        os.system('mv {0} {0}.tmp'.format(out_file))
-    with open(out_file, 'w') as out:
-        print(out_file)
-        out.write(r.text)
-
-# Post processing
-
 import re
-import xml.etree.ElementTree as ET
+from lxml import etree as ET
 
-file = base_path + config['multepal']['annotations_src'] #"multepal/annotations.xml" # THIS IS IN THE CONFIG!
-tree = ET.parse(file)
-root = tree.getroot()
+class Importer():
 
-amap = ET.SubElement(root, 'annotation-map')
+    def __init__(self, ini_file):
+        """Import configuratation and set up data structures"""
 
-def reformat_lb_id(lb_id):
-    lb_id = re.sub('Folio-', 'xom-f', lb_id)
-    lb_id = re.sub('recto', 's1', lb_id)
-    lb_id = re.sub('verso', 's2', lb_id)
-    lb_id = re.sub(' Column-A', '-quc', lb_id)
-    lb_id = re.sub(' Column-B', '-spa', lb_id)
-    lb_id = re.sub(' Line', '', lb_id)
-    lb_id = re.sub('Escolio-', 'xom_esc-f', lb_id)
-    lb_id = re.sub(' X', '', lb_id)
-    return lb_id
+        self.config = configparser.ConfigParser()
+        self.config.read(ini_file)
+        self.base_path = self.config['DEFAULT']['base_path']
+        self.multepal = self.config['multepal']['multepal_url']
+        
+        # Should use YAML instead if you want nested configs
+        self.exports = {
+            'topics': {}, 
+            'annotations': {}, 
+            'snippets': {}
+        }
+        for export in self.exports.keys():
+            self.exports[export] = {
+                'url': self.multepal + self.config['multepal'][f'{export}_url'],
+                'file': self.base_path + self.config['multepal'][f'{export}_src']
+            }
 
-for note in root.iter('annotation'): 
-    note.set('nid', note.find('nid').text)
-    lines = note.find('lineas')
-    if lines.text == None:
-        continue
+    def download(self):
+        """Download content from Drupal site"""
 
-    F = C = L = ''
-    newlines = {}
-    for item in lines.text.split('|'):
-        tokens = item.split()
-        key = tokens[0]
-        val = '-'.join(tokens)
-        if key in ('Folio', 'Escolio'):
-            if val != F:
-                F = val
-                C = ''
-                L = ''
-                newlines[F] = {}
-                if key == 'Escolio':
-                    newlines[F]['X'] = []
-        elif key == 'Column':
-            if val != C:
-                C = val
-                L = ''
-                newlines[F][C] = []
-        elif key == 'Line':
-            if L == val:
-                continue
-            L = val
-            if C == '':
-                C = 'X'
-            Ln = int(L.split('-')[1])
-            newlines[F][C].append('Line-{}'.format(Ln))
-            
-    newlineids = []            
-    for folio in newlines:
-        for col in newlines[folio]:
-            if len(newlines[folio][col]) == 0:
-                lb_id = "{} {}".format(folio, col)
-                newlineids.append(reformat_lb_id(lb_id))
-            for line in newlines[folio][col]:
-                lb_id = "{} {} {}".format(folio, col, line)
-                newlineids.append(reformat_lb_id(lb_id))
+        for export in self.exports.keys():
+            print(f"Getting {export} from Multepal site")
+            url = self.exports[export]['url']
+            r = requests.get(url)
+            out_file = self.exports[export]['file']
+            print('out_file', out_file)
+            if os.path.exists(out_file):
+                os.system(f'mv {out_file} {out_file}.tmp')
+            with open(out_file, 'w') as out:
+                print(out_file)
+                out.write(r.text)
 
-    #lines.text = ''
-    for line_id in newlineids:
-        #X = ET.SubElement(lines, 'line_id')
-        #X.text = line_id
-        Y = ET.SubElement(amap, 'item')
-        Y.set('nid', note.find('nid').text)
-        Y.set('line_id', line_id)
-    lines.set('reformatted','true')
+    def annotation_mapper(self):
+        """This method maps lines to IDs"""
 
-    for el_name in ['nid', 'lineas']:
-        el = note.find(el_name)
-        note.remove(el)
+        # Get annotation XML file path
+        file = self.exports['annotations']['file']
 
-tree.write(file, method='xml', encoding='utf-8')
+        # Create XML parser
+        parser = ET.XMLParser(remove_blank_text=True)
+        tree = ET.parse(file, parser=parser)
+        root = tree.getroot()
+
+        # Create a new element to store the annotation map
+        amap = ET.SubElement(root, 'annotation-map')
+
+        # Define translations of how lines are represented in Drupal vs in the XML.
+        # Todo: Replace this whole system so that Drupal is pulling from the IDs 
+        # as they exist in the source documents.
+        trans = {
+            'Folio': 'f',
+            'Escolio': 'f',
+            'recto': 's1',
+            'verso': 's2',
+            'A': 'quc',
+            'B': 'spa',
+        }
+
+        # Grab all the annotation elements
+        for note in root.xpath("//annotation"):
+
+            # Grab the node_id and mapped lines
+            node_id = note.xpath("nid/text()")[0]
+            lines = [el.xpath('li/text()') for el in note.xpath("lineas/ul")]
+
+            # Translate names using `trans` dict
+            for line in lines:
+                line_id = []
+                for el in line:
+                    parts = el.split()
+                    if el == 'MS 1515: Popol Wuj':
+                        line_id.append('xom')
+                    if el == 'MS 1515: Escolios':
+                        line_id.append('xom-esc')
+                    elif parts[0] == 'Folio' or parts[0] == 'Escolio':
+                        line_id.append(f'{trans[parts[0]]}{parts[1]}')
+                        line_id.append(f'{trans[parts[2]]}')
+                    elif parts[0] == 'Column':
+                        line_id.append(f'{trans[parts[1]]}')
+                    elif parts[0] == 'Line':
+                        line_id.append(parts[1])
+                line_id_str = '-'.join(line_id)
+                
+                # Add new element to XML doc in the followin pattern:
+                # <item nid="995" line_id="xom-f56-s1-quc-27" />
+                new_el = ET.SubElement(amap, 'item')
+                new_el.set('nid', node_id)
+                new_el.set('line_id', line_id_str)
+
+        tree.write(file, method='xml', encoding='utf-8', pretty_print=True)
+
+if __name__ == '__main__': 
+
+    import os
+    cwd = os.getcwd()
+    config_file = f"{cwd}/config.ini"
+    imp = Importer(config_file)
+    imp.download()
+    imp.annotation_mapper()
+
+
